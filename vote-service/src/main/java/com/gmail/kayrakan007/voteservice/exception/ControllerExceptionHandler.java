17c727ec29987383456191ba057ee02dd9f2c746
qk8 <kayrakan007@gmail.com>
2024-04-09 15:19:16 +0300

    handle VoteNotFoundException in vote-sevice

--- vote-service/src/main/java/com/gmail/kayrakan007/voteservice/exception/ControllerExceptionHandler.java
+++ vote-service/src/main/java/com/gmail/kayrakan007/voteservice/exception/ControllerExceptionHandler.java
@@ -13,46 +13,56 @@ import com.gmail.kayrakan007.commons.exception.ErrorMessage;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
-	
+
+	@ExceptionHandler(VoteNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ErrorMessage voteNotFoundException(VoteNotFoundException ex, WebRequest request) {
+
+		ErrorMessage errorMessage = new ErrorMessage(
+				HttpStatus.NOT_FOUND.value(),
+				Instant.now(),
+				ex.getMessage(),
+				request.getDescription(false));
+
+		return errorMessage;
+	}
+
 	@ExceptionHandler(VoteTypeNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorMessage voteTypeNotFoundException(VoteTypeNotFoundException ex, WebRequest request) {
-		
+
 		ErrorMessage errorMessage = new ErrorMessage(
 				HttpStatus.NOT_FOUND.value(),
 				Instant.now(),
 				ex.getMessage(),
-				request.getDescription(false)
-				);
-		
+				request.getDescription(false));
+
 		return errorMessage;
 	}
-	
+
 	@ExceptionHandler(PostNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorMessage postNotFoundException(PostNotFoundException ex, WebRequest request) {
-		
+
 		ErrorMessage errorMessage = new ErrorMessage(
 				HttpStatus.NOT_FOUND.value(),
 				Instant.now(),
 				ex.getMessage(),
-				request.getDescription(false)
-				);
-		
+				request.getDescription(false));
+
 		return errorMessage;
 	}
-	
+
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ErrorMessage exception(Exception ex, WebRequest request) {
-		
+
 		ErrorMessage errorMessage = new ErrorMessage(
 				HttpStatus.INTERNAL_SERVER_ERROR.value(),
 				Instant.now(),
 				ex.getMessage(),
-				request.getDescription(false)
-				);
-		
+				request.getDescription(false));
+
 		return errorMessage;
 	}
 
