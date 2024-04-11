8973a79f5e9f54f5b566a332200147a7ed3c9eb1
qk8 <kayrakan007@gmail.com>
2024-04-11 21:45:54 +0300

    add cache for TagServiceImpl

---
+++ tag-service/src/main/java/com/gmail/kayrakan007/tagservice/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package com.gmail.kayrakan007.tagservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Bean
+    RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+
+        return new LettuceConnectionFactory(redisConfig);
+    }
+
+    @Bean
+    RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+}
--- tag-service/src/main/java/com/gmail/kayrakan007/tagservice/service/implementation/TagServiceImpl.java
+++ tag-service/src/main/java/com/gmail/kayrakan007/tagservice/service/implementation/TagServiceImpl.java
@@ -3,6 +3,7 @@ package com.gmail.kayrakan007.tagservice.service.implementation;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.gmail.kayrakan007.tagservice.entity.Tag;
@@ -16,59 +17,81 @@ import com.gmail.kayrakan007.tagservice.service.TagService;
 @Service
 public class TagServiceImpl implements TagService {
 
-	TagRepository tagRepository;
-	TagMapper tagMapper;
+	private final TagRepository tagRepository;
+	private final TagMapper tagMapper;
+	private final RedisTemplate<String, TagResponse> redisTemplate;
 
-	public TagServiceImpl(TagRepository tagRepository, TagMapper tagMapper) {
+	public TagServiceImpl(TagRepository tagRepository, TagMapper tagMapper,
+			RedisTemplate<String, TagResponse> redisTemplate) {
 		this.tagRepository = tagRepository;
 		this.tagMapper = tagMapper;
+		this.redisTemplate = redisTemplate;
 	}
 
 	@Override
 	public List<TagResponse> getAllTags() {
 
-		List<Tag> tags = tagRepository.findAll();
-		List<TagResponse> tagResponses = tags.stream().map(tag -> tagMapper.tagToTagResponse(tag))
+		if (redisTemplate.hasKey("tagResponse"))
+			return redisTemplate.opsForList().range("tagResponse", 0, -1);
+
+		List<Tag> allTags = tagRepository.findAll();
+		List<TagResponse> allTagResponses = allTags.stream().map(tag -> tagMapper.tagToTagResponse(tag))
 				.collect(Collectors.toList());
-		
-		return tagResponses;
+
+		if (!allTagResponses.isEmpty())
+			redisTemplate.opsForList().rightPushAll("tagResponse", allTagResponses);
+
+		return allTagResponses;
 	}
 
 	@Override
 	public TagResponse getTagById(Long id) {
-		
+
+		if (redisTemplate.hasKey("tagResponse:" + id.toString()))
+			return redisTemplate.opsForValue().get("tagResponse:" + id.toString());
+
 		Tag tag = tagRepository.findById(id).orElseThrow(TagNotFoundException::new);
 		TagResponse tagResponse = tagMapper.tagToTagResponse(tag);
-		
+
+		redisTemplate.opsForValue().set("tagResponse:" + id.toString(), tagResponse);
+
 		return tagResponse;
 	}
 
 	@Override
 	public TagResponse createTag(TagRequest tagRequest) {
-		
+
 		Tag tag = tagMapper.tagRequestToTag(tagRequest);
 		Tag createdTag = tagRepository.save(tag);
 		TagResponse createdTagResponse = tagMapper.tagToTagResponse(createdTag);
-		
+
+		redisTemplate.delete("tagResponse");
+
 		return createdTagResponse;
 	}
 
 	@Override
 	public TagResponse updateTag(Long id, TagRequest tagRequest) {
-		
+
 		Tag tagFromDb = tagRepository.findById(id).orElseThrow(TagNotFoundException::new);
 		tagFromDb.setTagName(tagRequest.tagName());
 		Tag updatedTag = tagRepository.save(tagFromDb);
 		TagResponse tagResponse = tagMapper.tagToTagResponse(updatedTag);
-		
+
+		redisTemplate.delete("tagResponse");
+		redisTemplate.delete("tagResponse:" + id.toString());
+
 		return tagResponse;
 	}
 
 	@Override
 	public void deleteTag(Long id) {
-		
+
 		Tag tag = tagRepository.findById(id).orElseThrow(TagNotFoundException::new);
 		tagRepository.delete(tag);
+
+		redisTemplate.delete("tagResponse");
+		redisTemplate.delete("tagResponse:" + id.toString());
 
 	}
 
